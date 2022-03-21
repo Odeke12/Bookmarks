520fae4537648c898b367199cd8ae1bfee823936
Odeke12 <trevodex@gmail.com>
2022-03-21 21:55:10 +0300

    Unit test

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,55 @@
+import { AppModule } from './../app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from './../prisma/prisma.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { getPrismaClient } from '@prisma/client/runtime';
+
+describe('Auth service', () => {
+    let app: INestApplication
+    let service: AuthService
+    let prisma: PrismaService
+    let jwt: JwtService
+    let config: ConfigService
+    class AuthServiceMock{
+
+    }
+    jest.setTimeout(10000)
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile()
+
+        app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(new ValidationPipe({
+            whitelist: true
+          }));
+
+          await app.init()
+          prisma = app.get(PrismaService)
+          jwt = app.get(JwtService)
+          config = app.get(ConfigService)
+        service = new AuthService(prisma, jwt, config)
+      });
+      it('should be defined', () => {
+        expect(service).toBeDefined();
+    })
+
+    describe('Check connection',() => {
+        it('Should sign up a new user',async() => {
+           const dto:AuthDto = {
+               email: 'timo@gmail.com',
+               password: 'timothy'
+           }
+           const token = await service.signup(dto)
+
+          expect(token).toEqual({
+            access_token: expect.any(String),
+        })
+        })
+    })
+})
+
